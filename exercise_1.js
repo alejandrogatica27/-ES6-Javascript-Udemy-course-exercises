89b0fd55a38713ffd81d87fbc8e0af3099cc979c
alejandrogatica27 <86114797+alejandrogatica27@users.noreply.github.com>
2022-06-12 23:35:12 -0600

    Update exercise_1.js

--- exercise_1.js
+++ exercise_1.js
@@ -1,2 +1,10 @@
-
+function handlePosts() {
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+    
+    posts.forEach(post => savePost(post))
+}
 
